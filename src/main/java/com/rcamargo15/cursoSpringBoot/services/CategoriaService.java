db441a2ec624b453bf61e9d8253184af2496960a
RCamargo15 <rafael.camargo384@gmail.com>
2022-04-25 15:05:06 -0300

    Tratamento de exceção e ajuste no categoriaResource/Service

--- src/main/java/com/rcamargo15/cursoSpringBoot/services/CategoriaService.java
+++ src/main/java/com/rcamargo15/cursoSpringBoot/services/CategoriaService.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.rcamargo15.cursoSpringBoot.domain.Categoria;
 import com.rcamargo15.cursoSpringBoot.repositories.CategoriaRepository;
+import com.rcamargo15.cursoSpringBoot.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo:" + Categoria.class.getName()));
 	}
+
 }
