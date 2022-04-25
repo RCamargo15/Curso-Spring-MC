ddf4aadce8f5ee2f5f919ed58cc88b53c82ef5d4
RCamargo15 <rafael.camargo384@gmail.com>
2022-04-25 14:02:01 -0300

    Produto e associação muitos para muitos entre classes

--- src/main/java/com/rcamargo15/cursoSpringBoot/CursoSpringBootApplication.java
+++ src/main/java/com/rcamargo15/cursoSpringBoot/CursoSpringBootApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rcamargo15.cursoSpringBoot.domain.Categoria;
+import com.rcamargo15.cursoSpringBoot.domain.Produto;
 import com.rcamargo15.cursoSpringBoot.repositories.CategoriaRepository;
+import com.rcamargo15.cursoSpringBoot.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringBootApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringBootApplication.class, args);
@@ -25,7 +30,16 @@ public class CursoSpringBootApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		p1.setCategorias(Arrays.asList(cat1));
+		p2.setCategorias(Arrays.asList(cat1, cat2));
+		p3.setCategorias(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
